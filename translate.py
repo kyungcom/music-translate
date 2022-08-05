d5b941eaa74309652b655fd308f1a89446868c94
ParkInHo <qrdlsgh1234@khu.ac.kr>
2022-08-05 23:46:37 +0900

    add TranslateModule

---
+++ translate.py
@@ -0,0 +1,33 @@
+import json
+import config
+import searchMusic
+import requests, json
+
+lyrics_url = "https://openapi.naver.com/v1/papago/n2mt"
+lan_url = "https://openapi.naver.com/v1/papago/detectLangs"
+
+searchMusic.init()
+
+lan_params = {
+    "query" : searchMusic.lyric
+}
+
+headers = {
+     "Content-Type": "application/json",
+     "X-Naver-Client-Id": config.CLIENT_ID,
+     "X-Naver-Client-Secret": config.CLIENT_SECRET,
+}
+
+response = requests.post(lan_url, json.dumps(lan_params), headers=headers)
+if response.json()["langCode"] == "ko":
+    print(searchMusic.lyric)
+    
+else:
+    lyrics_params = {
+        "source" : response.json()["langCode"],
+        "target" : "ko",
+        "text" : searchMusic.lyric,
+    }
+
+    response = requests.post(lyrics_url, json.dumps(lyrics_params), headers=headers)
+    print(response.json()["message"]["result"]["translatedText"])
